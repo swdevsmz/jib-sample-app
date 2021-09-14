dd41c32bd27ddcc129de7495b348b0c4c3577ebd
swdevsmz <swdev.smz@gmail.com>
2021-09-14 22:39:33 +0900

    初期アプリーケーションの追加

---
+++ src/main/java/com/example/jibsampleapp/JibSampleAppApplication.java
@@ -0,0 +1,13 @@
+package com.example.jibsampleapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JibSampleAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JibSampleAppApplication.class, args);
+	}
+
+}
